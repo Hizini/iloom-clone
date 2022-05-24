e74585fcf7dbed4f9908669d97ae6c618161139f
KimHeeJin <ejfdnrl@naver.com>
2022-05-24 14:49:56 +0900

    InstagramArea 이미지 주소 변경

--- src/Pages/Home/InstagramArea/index.js
+++ src/Pages/Home/InstagramArea/index.js
@@ -6,23 +6,23 @@ class InstagramArea extends Component {
     renderItems = () => {
         const items = [
             {
-                src: 'https://scontent-nrt1-1.cdninstagram.com/v/t51.29350-15/280513917_541328510737606_6503693878957756736_n.jpg?_nc_cat=110&ccb=1-6&_nc_sid=8ae9d6&_nc_ohc=vDkyE9PKz3sAX_tSw_7&_nc_ht=scontent-nrt1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT_SvEMMfDS6T-d8gR1PsSmTJANMf9BAGziaT9Q-IMF9Sg&oe=6283F7A6',
+                src: 'https://scontent-nrt1-1.cdninstagram.com/v/t51.29350-15/280513917_541328510737606_6503693878957756736_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=UW9P7ODu5V8AX-G5KlP&_nc_ht=scontent-nrt1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT9XZNe6j-vkeydAutX_JfRlbVkpC5e7UwvpRsKWnvkWkQ&oe=6291CF66',
                 movePath: 'https://www.instagram.com/tv/Cde6LinFIBu/'
             },
             {
-                src: 'https://scontent-nrt1-1.cdninstagram.com/v/t51.29350-15/280273655_1068353417363358_6823321364398367076_n.jpg?_nc_cat=109&ccb=1-6&_nc_sid=8ae9d6&_nc_ohc=NogWlJnC2egAX-VGq-d&_nc_ht=scontent-nrt1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT-UoaZ0lDTRTkCTSL5ij4rXoi4h49L-3Eg9bCdBYtdjaQ&oe=628340A1',
+                src: 'https://scontent-nrt1-1.cdninstagram.com/v/t51.29350-15/280973527_740063930347553_1359569032736320515_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=T5hyguKqBpYAX_U5GHY&_nc_ht=scontent-nrt1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT-pvyInTv59pnJrmjGcplnKX7iEKV4dL-Al7grlmNHH4A&oe=62907ED2',
                 movePath: 'https://www.instagram.com/p/CdajmLsvBQh/'
             },
             {
-                src: 'https://scontent-nrt1-1.cdninstagram.com/v/t51.29350-15/280176316_1164956201015638_1335133662676070842_n.jpg?_nc_cat=101&ccb=1-6&_nc_sid=8ae9d6&_nc_ohc=LPeS62Z4OVEAX_Fr_zE&_nc_ht=scontent-nrt1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT8gXC2uYmo6-m4I2P5y8QKXOxVmH_1VLEDSpBPysUK_Sw&oe=62841025',
+                src: 'https://scontent-nrt1-1.cdninstagram.com/v/t51.29350-15/281845033_5426081007429544_6122958056969853267_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=QapkcxYeDPkAX9L_7PY&_nc_ht=scontent-nrt1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT8ocN1HgF5ipaqj3KDBuW7fHHvxx4fbxOitmynyJYq3MQ&oe=62916B69',
                 movePath: 'https://www.instagram.com/p/CdXuZozvwCL/'
             },
             {
-                src: 'https://scontent-nrt1-1.cdninstagram.com/v/t51.29350-15/280384315_1192990924816168_5609485126659171563_n.jpg?_nc_cat=102&ccb=1-6&_nc_sid=8ae9d6&_nc_ohc=smzBAf6LPD4AX9fO8mQ&_nc_ht=scontent-nrt1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT9nmIT7IcyOQPBh9PvWEAFPKxpuTsoz0KnxyFioWPAhww&oe=62837D65',
+                src: 'https://scontent-nrt1-1.cdninstagram.com/v/t51.29350-15/282110910_703455914295102_1278631639380186151_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=e6GgJmNlM6sAX8NdZ8N&_nc_ht=scontent-nrt1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT8jzFwLgB1wy6WIOzOZ2byk3Leo_isS9wZgIO0XrWZagA&oe=6291CC48',
                 movePath: 'https://www.instagram.com/p/CdVHK8dv2ck/'
             },
             {
-                src: 'https://scontent-nrt1-1.cdninstagram.com/v/t51.29350-15/280079980_321444740070293_5890346277793620777_n.jpg?_nc_cat=100&ccb=1-6&_nc_sid=8ae9d6&_nc_ohc=YN2mxg9gYoQAX88_UxD&_nc_ht=scontent-nrt1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT8TZ4-nbrWkZeWGRKrUpBEemywDOqArbapQpUUmiqvu8g&oe=6283C285',
+                src: 'https://scontent-nrt1-1.cdninstagram.com/v/t51.29350-15/283515017_383807513765744_2094116726991937192_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=HmMcQNG-OPwAX9bKIQM&_nc_ht=scontent-nrt1-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT-RP4YgmJz-dpt7VOyfTz6C3DiSzrXoP8fgflYC0xD7QA&oe=6290D442',
                 movePath: 'https://www.instagram.com/p/CdKRmnYPBJg/'
             }
         ]
