7238903bd330c5591fa958fb75ae412dcb86931d
Hizini <ejfdnrl@naver.com>
2022-06-07 20:39:48 +0900

    CardSection 이미지 추가 및 경로 수정

--- src/Pages/Home/CardSection/index.js
+++ src/Pages/Home/CardSection/index.js
@@ -12,13 +12,13 @@ class CardSection extends Component {
     renderItems = () => {
         const items = [
             {
-                src: 'https://cdn.iloom.com/upload/contents/20220323/_4f475cb6-47d2-4208-bb69-305397a6d91c.jpg'
+                src: '/images/home/card1.jpg'
             },
             {
-                src: 'https://cdn.iloom.com/upload/contents/20220401/_e6f26110-f564-4d55-9790-46f4227f1428.jpg'
+                src: '/images/home/card2.jpg'
             },
             {
-                src: 'https://cdn.iloom.com/upload/contents/20220316/_b657a84d-c3cf-4c5b-8a89-87fb85d950ed.jpg'
+                src: '/images/home/card3.jpg'
             },
         ]
         return items.map(item => {
